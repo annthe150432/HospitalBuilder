104bf3582382460f2c753514bf5524bfb48f43e8
annthe150432 <annthe150432@gmail.com>
2022-07-16 16:17:02 +0700

    draw tile

--- Assets/Scripts/GridSystem/GridSystem.cs
+++ Assets/Scripts/GridSystem/GridSystem.cs
@@ -19,7 +19,7 @@ public class GridSystem
             {
                 lock (locker)
                 {
-                    _instance = new GridSystem(50, 30, 1);
+                    _instance = new GridSystem(22, 3, 1);
                 }
             }
             return _instance;
@@ -31,9 +31,9 @@ public class GridSystem
         cols = width;
         rows = height;
         this.tileSize = tileSize;
-        for (int i=0; i<cols; i++)
+        for (int i=-4; i<cols; i++)
         {
-            for (int j=0; j<rows; j++)
+            for (int j=-11; j<rows; j++)
             {
                 Tuple<int, int> key = new Tuple<int, int>(i, j);
                 Tile tile = new Tile(i*tileSize, j*tileSize);
